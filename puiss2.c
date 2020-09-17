d74fa434ec66f6e3f769e180ae7d10bd52a8adac
Leo_Delas <kay1900zz@gmail.com>
2020-09-17 20:30:17 +0200

    Exos_TP1

--- puiss2.c
+++ puiss2.c
@@ -30,22 +30,15 @@ int puissance2(int x, int k)
             
             k = k / 2;
             resultat *= resultat;
-            printf("k est egal : %d\n", k);
-            printf("== 0 temp est egal : %d\n\n", resultat);
         }
 
         else if (k % 2 != 0)
-        {
-            printf("k est egal : %d\n", k);
-            
+        {   
             k /= 2;
             temp = temp * resultat;
             resultat *= resultat;
-            
-            printf("!=0 temp est egal : %d\n\n", resultat);
-
         }
     }
     
-    return printf("%d\n", resultat*temp);
+    return printf("Voici le resultat : %d\n", resultat*temp);
 }
